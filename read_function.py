da41312d39e19d84e14570821e512e9334e0ace4
fadiloderinu <99251779+fadiloderinu@users.noreply.github.com>
2025-07-25 13:10:27 -0400

    Create read_function.py

---
+++ read_function.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('HighAvailabilityTable')
+
+def lambda_handler(event, context):
+    try:
+        response = table.scan()
+        items = response['Items']
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',  # Enable CORS
+            },
+            'body': json.dumps(items)  # Return the items array
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',  # Enable CORS
+            },
+            'body': json.dumps({'error': str(e)})
+        }
